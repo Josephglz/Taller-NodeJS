ea1fee63354b635007011b1213aab63df8892aff
JosephGlzPro <115588982+JosephGlzPro@users.noreply.github.com>
2022-11-26 17:21:31 -0600

    Update datosConexion.js

--- db/datosConexion.js
+++ db/datosConexion.js
@@ -1,9 +1,8 @@
 const configMySQL = {
-    host: 'localhost',
-    user: 'root',
-    password: 'prointernet',
-    database: 'proyecto_tareas',
-    insecureAuth: true
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b0419ba3f718ca',
+    password: '85ed2574',
+    database: 'heroku_ea404fe62a9b42a'
 }
 
 module.exports = configMySQL;
